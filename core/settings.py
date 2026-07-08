be46266959b2a9bf63d682ce08ba8637e75450e2
sandun18 <sandunanjana333@gmail.com>
2026-07-08 13:37:17 +0530

    fix: Login page changes, frontend and backend

--- core/settings.py
+++ core/settings.py
@@ -50,7 +50,7 @@ DATABASES = {
         'USER': 'root',
         'PASSWORD': '',
         'HOST': 'localhost',
-        'PORT': '3306',
+        'PORT': '3307',
         'OPTIONS': {'charset': 'utf8mb4'},
     }
 }
