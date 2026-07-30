475ce51c14ae848a2eabf494e2f02b4f8acdb23d
Nuwan Ganganath <nuwanrathnayaka53@gmail.com>
2026-07-30 13:09:17 +0530

    increase access token time to 8 hrs

--- core/settings.py
+++ core/settings.py
@@ -86,6 +86,20 @@ REST_FRAMEWORK = {
     ],
 }
 
+
+from datetime import timedelta
+
+
+SIMPLE_JWT = {
+    "ACCESS_TOKEN_LIFETIME": timedelta(hours=8),
+    "REFRESH_TOKEN_LIFETIME": timedelta(days=30),
+
+    "ROTATE_REFRESH_TOKENS": True,
+    "BLACKLIST_AFTER_ROTATION": True,
+
+    "UPDATE_LAST_LOGIN": True,
+}
+
 import os
 
 # For daily development / demos where you just need to SEE it worked:
