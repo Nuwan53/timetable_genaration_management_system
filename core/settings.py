fd21219207d20cf98cd20849c773b10f21cbb275
Nuwan Ganganath <nuwanrathnayaka53@gmail.com>
2026-08-02 15:04:24 +0530

    changes/06

--- core/settings.py
+++ core/settings.py
@@ -51,7 +51,7 @@ DATABASES = {
         'USER': 'root',
         'PASSWORD': '',
         'HOST': 'localhost',
-        'PORT': '3307',
+        'PORT': '3306',
         'OPTIONS': {'charset': 'utf8mb4'},
     }
 }
